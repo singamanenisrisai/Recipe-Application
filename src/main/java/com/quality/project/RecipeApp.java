e91c5f96de7184b6bbc6723d9e3850efd142cd30
NavneetPrakashSingh <navneet_prakash_singh@live.com>
2019-02-25 18:10:41 -0400

    Revert "Pending error: Unable to fix why controllers are not working on azure"
    This reverts commit 82f64ba80a7fca7efa4982c94679371db9f3bd66.

--- src/main/java/com/quality/project/RecipeApp.java
+++ src/main/java/com/quality/project/RecipeApp.java
@@ -1,20 +1,11 @@
 package com.quality.project;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-public class RecipeApp extends SpringBootServletInitializer{
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(RecipeApp.class);
-	}
+public class RecipeApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- src/main/java/com/quality/project/controller/RecipeController.java
+++ src/main/java/com/quality/project/controller/RecipeController.java
@@ -1,34 +1,39 @@
-package com.quality.project.search;
+package com.quality.project.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.quality.project.search.Search;
+
 @RestController
-public class SearchController {
+public class RecipeController {
 	
 	@Autowired
 	Search sampleResults;
 	
-	@RequestMapping("/sample")
-	public String sample() {
-		return "In Sample";
+	@RequestMapping("/navneet")
+	public String search() {
+		return "Hello";
 	}
 	
-	@RequestMapping("/searchfield")
+	@RequestMapping("/search")
 	public List<String> getAllUserName() {
 		return sampleResults.getAllResults();
 	}
 	
-	@RequestMapping("/home")
+	@GetMapping(value = "/home")
 	public ModelAndView helloWorld() {
 	    ModelAndView mav = new ModelAndView();
 	    mav.setViewName("home");
 	    mav.addObject("message", sampleResults.getAllResults());
 	    return mav;
 	}
+
 }
